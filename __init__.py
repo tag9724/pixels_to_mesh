8dfe40f339bf509daf0cc4d13b30e91b0922ae9b
Gaëtan Burg <gaetanburg@gmail.com>
2022-02-13 02:23:43 +0100

    v1.0.0

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "tag9724",
     "description": "Convert an image to a mesh",
     "blender": (2, 80, 0),
-    "version": (0, 0, 1),
+    "version": (1, 0, 0),
     "location": "",
     "warning": "",
     "support": "COMMUNITY",
